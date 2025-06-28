0530e8c8af8db4c7d37dd98661325d195ca8e09c
caopham123 <caopt2001@gmail.com>
2025-06-28 15:39:35 +0700

    read real-time video

--- container_detection.py
+++ container_detection.py
@@ -1,12 +1,19 @@
 from ultralytics import YOLO
 import cv2, os
-from setting import MODEL_DETECTION
+import torch
+import timm
+import torch.nn as nn
+from torchvision import transforms
+from PIL import Image   
+import time
+from setting import MODEL_DETECTION, CONF_DETECT_THRESH, FPS
+from goods_classification import load_model_classification, get_class_name, preprocess_image
 
 MARGIN = 50
 model_file = os.path.join(MODEL_DETECTION, "best.pt")
 
-model = YOLO(model_file.replace('\\', '/'))
-class_names = model.names
+model_detection = YOLO(model_file.replace('\\', '/'))
+class_names = model_detection.names
 
 
 def translate_label(idx_class):
@@ -17,7 +24,7 @@ def translate_label(idx_class):
 
 def detect_container_img(url):
     pic = cv2.imread(url)
-    results = model(pic)
+    results = model_detection(pic)
 
     for box in results[0].boxes:
         x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -46,15 +53,104 @@ def detect_container_img(url):
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-def detect_container_video(url):
+def real_time_video(url):
+    model_classification, class_to_idx = load_model_classification()
+
     cap = cv2.VideoCapture(url)
+    frame_order = 0
+    frame_counter = 0
     while cap.isOpened():
         ret, pic = cap.read()
         if not ret: break
 
-        results = model(pic)
-        for box in results[0].boxes:
+        if frame_order % FPS == 0:
+            t = time.time()
+            frame_counter += 1
+            print(f"\n\n==============\n Frame {frame_counter}")
+            # pic = cv2.resize(pic, (600, 450))
+            h, w = pic.shape[:2]
+            results = model_detection(pic)
+            masks = results[0].boxes.conf > CONF_DETECT_THRESH
+            filtered_conf = results[0].boxes[masks]
+
+            for i, box in enumerate(filtered_conf):
+                print(f"    ======\n  Box conf {i}. of frame {frame_counter}")
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                print(f"x1, y1, x2, y2 {x1, y1, x2, y2}")
+                # Get class ID
+                cls_id = int(box.cls[0])
+
+                # Get class name
+                label_container = translate_label(cls_id)
+
+                # Optional: Get confidence score
+                conf_detection = float(box.conf[0])
+
+                # Draw bounding box
+                cv2.rectangle(pic, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                x1 = max(0, x1 - MARGIN)
+                y1 = max(0, y1 - MARGIN)
+                x2 = min(w, x2 + MARGIN)
+                y2 = min(h, y2 + MARGIN)
+                cropped_pic = pic[y1: y2, x1: x2]
+                print(f"crop size: {cropped_pic.shape[:2]}")
+                # cv2.imshow("crop_pic",cropped_pic)
+                
+                pil_img = Image.fromarray(cv2.cvtColor(cropped_pic, cv2.COLOR_BGR2RGB))
+                input_tensor = preprocess_image(pil_img)
+
+                # Inference image
+                with torch.no_grad():
+                    outputs = model_classification(input_tensor)
+                    probability = nn.functional.softmax(outputs[0], dim=0)
+                    best_prob, best_class = torch.max(probability, 0)
+                
+                print(f"Best prob: {best_prob}      Match class idx: {best_class}")
+
+                # Get class name and confidence
+                goods_name = get_class_name(best_class.item(), class_to_idx)
+                print(f"goods label {goods_name}")
+                conf_goods = best_prob.item()*100.
+                conf_goods = round(conf_goods, 2)
+                label = f"{goods_name}: {conf_goods}%"
+
+                # Display prediction on frame
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                font_scale = .6
+                thickness = 2
+                margin = 10
+                (label_w, label_h), _ = cv2.getTextSize(label, font, font_scale, thickness)
+                x = w - label_w - margin
+                y = label_h + margin
+                print(f"label_classification: {label}")
+                cv2.putText(pic, label, (x, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
+                cv2.putText(pic, label_container, (margin, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
+
+                # Display the resulting frame
+                cv2.imshow('Classify goods', pic)
+            print(time.time() - t)
+        frame_order += 1
+        if cv2.waitKey(25) & 0xFF == ord('q'): break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def detect_container_video(url):
+    cap = cv2.VideoCapture(url)
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+    frame_counter = 0
+    while cap.isOpened():
+        ret, pic = cap.read()
+        if not ret: break
+        frame_counter += 1
+        results = model_detection(pic)
+        masks = results[0].boxes.conf > CONF_DETECT_THRESH
+        filtered_conf = results[0].boxes[masks]
+        for i, box in enumerate(filtered_conf):
+            print(f"    ======\n  Box conf {i}. of frame {frame_counter}")
             x1, y1, x2, y2 = map(int, box.xyxy[0])
+            print(f"x1, y1, x2, y2 {x1, y1, x2, y2}")
             # Get class ID
             cls_id = int(box.cls[0])
 
@@ -68,25 +164,20 @@ def detect_container_video(url):
             cv2.rectangle(pic, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cropped_pic = pic[y1 -MARGIN: y2 +MARGIN, x1 -MARGIN: x2 +MARGIN]
             # Draw label
-            # label = f"{cls_name} {conf:.2f}"
-            # cv2.putText(frame, label, (x1, y1 - 10),
-            #         cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 4)
+            label = f"{cls_name} {conf:.2f}"
+            cv2.putText(pic, label, (x1, y1 + 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
         resized_pic = cv2.resize(pic, (500, 400))
-    #     cv2.imshow("Container Detection", resized_pic)
-    #     if cv2.waitKey(1) == 27: break
+        cv2.imshow("Container Detection", resized_pic)
+        if cv2.waitKey(25) & 0xFF == ord('q'): break
 
-    # cap.release()
-    # cv2.destroyAllWindows()
-    return resized_pic, cropped_pic, cls_name
+    cap.release()
+    cv2.destroyAllWindows()
+    # return resized_pic, cropped_pic, cls_name
 
 if __name__ == "__main__":
-    print(type(model_file))
-    print(model_file.replace('\\','/'))
-    url_input = detect_container_img("./data/img01.jpg")
-    # url_input = pred_video("video_03.mp4")
-    # pic = cv2.imread("./data/img01.jpg")
-    # crop_img = pic[500-50: 1000, 100: 600]
-    # cv2.imshow("demo", crop_img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    real_time_video("rtsp://admin:longson2016@192.168.10.26:1555/Streaming/Channels/5002")
+    # detect_container_video("./data/video_01.mp4")
+
+    # url_input = detect_container_img("./data/img01.jpg")
--- goods_classification.py
+++ goods_classification.py
@@ -6,7 +6,7 @@ from torchvision import transforms
 from PIL import Image   
 from main2 import create_model
 from setting import *
-from container_detection import detect_container_img, detect_container_video
+# from container_detection import detect_container_img, detect_container_video
 
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -43,114 +43,112 @@ def preprocess_image(image):
 def get_class_name(class_idx, class_to_idx):
     # Convert class key to index key
     idx_to_class = {int(v): k for k, v in class_to_idx.items()}
-    print(f"idx_to_class: {idx_to_class}")
+    # print(f"idx_to_class: {idx_to_class}")
     class_name = idx_to_class.get(int(class_idx), "Ko_xac_dinh")
     class_name = class_name.replace('_', ' ')
-    print(f"Result: {class_name}")
+    # print(f"Result: {class_name}")
     
     return class_name
 
-def classify_goods_video(video_url):
-    resized_frame, cropped_frame, label_container = detect_container_video(video_url)
+# def classify_goods_video(video_url):
+#     model, class_to_idx = load_model_classification()
+
+#     cap = cv2.VideoCapture(video_url)
+#     if not cap.isOpened():
+#         raise IOError("Not found video")
     
-    model, class_to_idx = load_model_classification()
+#     while True:
+#         ret, frame = cap.read()
+#         if not ret: break
 
-    cap = cv2.VideoCapture(video_url)
-    if not cap.isOpened():
-        raise IOError("Not found video")
-    
-    while True:
-        ret, frame = cap.read()
-        if not ret: break
+#         h, w = frame.shape[:2]
+#         print(h, w)
 
-        h, w = frame.shape[:2]
-        print(h, w)
+#         pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+#         input_tensor = preprocess_image(pil_img)
 
-        pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        input_tensor = preprocess_image(pil_img)
+#         # Inference image
+#         with torch.no_grad():
+#             outputs = model(input_tensor)
+#             probability = nn.functional.softmax(outputs[0], dim=0)
+#             best_prob, best_class = torch.max(probability, 0)
+#         print(f"Best prob: {best_prob}\nMatch class idx: {best_class}")
 
-        # Inference image
-        with torch.no_grad():
-            outputs = model(input_tensor)
-            probability = nn.functional.softmax(outputs[0], dim=0)
-            best_prob, best_class = torch.max(probability, 0)
-        print(f"Best prob: {best_prob}\nMatch class idx: {best_class}")
+#         # Get class name and confidence
+#         class_name = get_class_name(best_class.item(), class_to_idx)
+#         print(f"cl_name {class_name}")
+#         confidence = best_prob.item()*100.
+#         confidence = round(confidence, 2)
+#         label = f"{class_name}: {confidence}%"
 
-        # Get class name and confidence
-        class_name = get_class_name(best_class.item(), class_to_idx)
-        print(f"cl_name {class_name}")
-        confidence = best_prob.item()*100.
-        confidence = round(confidence, 2)
-        label = f"{class_name}: {confidence}%"
-
-        # Display prediction on frame
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = .6
-        thickness = 2
-        margin = 10
-        (label_w, label_h), _ = cv2.getTextSize(label, font, font_scale, thickness)
-        x = w - label_w - margin
-        y = label_h + margin
-        print (f"w - h {x}-{y}")
-        print(f"label: {label}")
-        cv2.putText(resized_frame, label, (x, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
-        cv2.putText(resized_frame, label_container, (margin, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
+#         # Display prediction on frame
+#         font = cv2.FONT_HERSHEY_SIMPLEX
+#         font_scale = .6
+#         thickness = 2
+#         margin = 10
+#         (label_w, label_h), _ = cv2.getTextSize(label, font, font_scale, thickness)
+#         x = w - label_w - margin
+#         y = label_h + margin
+#         print (f"w - h {x}-{y}")
+#         print(f"label: {label}")
+#         cv2.putText(resized_frame, label, (x, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
+#         cv2.putText(resized_frame, label_container, (margin, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
         
-        # Display the resulting frame
-        # resized_frame = cv2.resize(frame, (800, 600))
-        cv2.imshow('Webcam Classification', resized_frame)
+#         # Display the resulting frame
+#         # resized_frame = cv2.resize(frame, (800, 600))
+#         cv2.imshow('Webcam Classification', resized_frame)
         
-        # Break loop on 'q' key
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+#         # Break loop on 'q' key
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
     
-    cap.release()
-    cv2.destroyAllWindows()
+#     cap.release()
+#     cv2.destroyAllWindows()
 
-def classify_goods_img(img_path):
-    resized_pic, cropped_pic, label_container = detect_container_img(img_path)
-    h, w = resized_pic.shape[:2]
+# def classify_goods_img(img_path):
+#     resized_pic, cropped_pic, label_container = detect_container_img(img_path)
+#     h, w = resized_pic.shape[:2]
 
-    model, class_to_idx = load_model_classification()
+#     model, class_to_idx = load_model_classification()
 
-    pil_img = Image.fromarray(cv2.cvtColor(cropped_pic, cv2.COLOR_BGR2RGB))
-    input_tensor = preprocess_image(pil_img)
+#     pil_img = Image.fromarray(cv2.cvtColor(cropped_pic, cv2.COLOR_BGR2RGB))
+#     input_tensor = preprocess_image(pil_img)
 
-    # Inference image
-    with torch.no_grad():
-        outputs = model(input_tensor)
-        probability = nn.functional.softmax(outputs[0], dim=0)
-        best_prob, best_class = torch.max(probability, 0)
+#     # Inference image
+#     with torch.no_grad():
+#         outputs = model(input_tensor)
+#         probability = nn.functional.softmax(outputs[0], dim=0)
+#         best_prob, best_class = torch.max(probability, 0)
     
-    print(f"Best prob: {best_prob}\nMatch class idx: {best_class}")
+#     print(f"Best prob: {best_prob}\nMatch class idx: {best_class}")
 
-    # Get class name and confidence
-    class_name = get_class_name(best_class.item(), class_to_idx)
-    print(f"cl_name {class_name}")
-    confidence = best_prob.item()*100.
-    confidence = round(confidence, 2)
-    label = f"{class_name}: {confidence}%"
-    # Display prediction on frame
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = .6
-    thickness = 2
-    margin = 10
-    (label_w, label_h), _ = cv2.getTextSize(label, font, font_scale, thickness)
-    x = w - label_w - margin
-    y = label_h + margin
-    print (f"w - h {x}-{y}")
-    print(f"label: {label}")
-    cv2.putText(resized_pic, label, (x, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
-    cv2.putText(resized_pic, label_container, (margin, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
-    # Display the resulting frame
-    cv2.imshow('Classify goods', resized_pic)
+#     # Get class name and confidence
+#     class_name = get_class_name(best_class.item(), class_to_idx)
+#     print(f"cl_name {class_name}")
+#     confidence = best_prob.item()*100.
+#     confidence = round(confidence, 2)
+#     label = f"{class_name}: {confidence}%"
+#     # Display prediction on frame
+#     font = cv2.FONT_HERSHEY_SIMPLEX
+#     font_scale = .6
+#     thickness = 2
+#     margin = 10
+#     (label_w, label_h), _ = cv2.getTextSize(label, font, font_scale, thickness)
+#     x = w - label_w - margin
+#     y = label_h + margin
+#     print (f"w - h {x}-{y}")
+#     print(f"label: {label}")
+#     cv2.putText(resized_pic, label, (x, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
+#     cv2.putText(resized_pic, label_container, (margin, y), fontFace=font, fontScale=font_scale, color=(0, 255, 0), thickness=thickness)
+#     # Display the resulting frame
+#     cv2.imshow('Classify goods', resized_pic)
     
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     
-    classify_goods_video("./data/video_01.mp4")
+    # classify_goods_video("./data/video_01.mp4")
     # classify_goods_img('./data/img01.jpg')
     # class_to_idx = {'class01': 0, 'class02': 1}
     # get_class_name(10, class_to_idx)
